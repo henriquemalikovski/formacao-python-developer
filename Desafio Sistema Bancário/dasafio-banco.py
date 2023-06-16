9ad1e871da3af4dcd28f892e6b7711922df1a7fd
Henrique Malikovski <hmalikovski@gmail.com>
2023-06-16 20:12:56 +0000

    Ajustes

--- Desafio Sistema Bancário/dasafio-banco.py
+++ Desafio Sistema Bancário/dasafio-banco.py
@@ -3,6 +3,8 @@ menu = """
 [D] Depositar
 [S] Sacar
 [E] Extrato
+[C] Cadastrar Cliente
+[I] Cadastrar Conta
 [Q] Sair
 
 """
@@ -13,17 +15,90 @@ extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
+clients = {
+    "02132366055": { 
+        "name": "Henrique Malikovski", 
+        "date_of_birth": "27/01/1995",
+        "address": {
+            "road": "Barão do Trinfo", 
+            "number": 2150, 
+            "complement": "Apto 37", 
+            "district": "Cruzeiro",
+            "city": "Venâncio Aires",
+            "state": "RS",
+            "cep": 95800000
+        },
+    },
+    "02132366000": { 
+        "name": "Henrique Malikovski", 
+        "date_of_birth": "27/01/1995",
+        "address": {
+            "road": "Barão do Trinfo", 
+            "number": 2150, 
+            "complement": "Apto 37", 
+            "district": "Cruzeiro",
+            "city": "Venâncio Aires",
+            "state": "RS",
+            "cep": 95800000
+        },
+    },
+}
+
+contas = {
+    "0001": {
+        1:{
+            "cliente": "02132366055",
+            "saldo": 1000,
+            "extrato": ""
+        },
+    },
+}
+
+def validateCPF(pCPF):
+    global clients
+    for chave in clients:
+        if chave == pCPF:
+            return False
+    
+    return True
+
+def add_new_client(pCPF, pName, pDate_of_birth, pAddress):
+    global clients
+    clients[pCPF] = dict(name=pName, date_of_birth = pDate_of_birth, address = pAddress)
+
+def adicionaContaCorrente(pCPF, pSaldo):
+    global contas
+    conta = dict(cliente=pCPF, saldo=pSaldo, extrato="")
+    
+
 
 def addMensagem(valor, operacao):
-    return '{} no valor de R$ {:.2f}'.format(
-        operacao.upper(), valor) + '\n'
+    return "{} no valor de R$ {:.2f}".format(
+        operacao.upper(), valor) + "\n"
 
 
 while True:
     opcao = str(input(menu)).upper()
-
     if opcao == "Q":
         break
+
+    if opcao == "C":
+        cpf = str(input("Digite o CPF: (apenas números)"))
+        if not validateCPF(cpf):
+            raise Exception("Cliente já cadastrado no sistema! Por favor verifique!")    
+
+        nome = str(input("Digite o nome completo do cliente: "))    
+        date_of_birth = str(input("Digite a data de nascimento: "))
+
+        address = {}
+        address["road"] = str(input("Digite a Rua do Endereço: "))
+        address["number"] = str(input("Digite Número do Endereço: "))
+        address["complement"] = str(input("Digite o Complemento do Endereço: "))
+        address["district"] = str(input("Digite o Bairro do Endereço: "))
+        address["city"] = str(input("Digite a Cidade do Endereço: "))
+        address["state"] = str(input("Digite o Estado do Endereço: "))
+        address["cep"] = str(input("Digite o CEP do Endereço: "))
+        add_new_client(cpf, nome, date_of_birth, address)
     elif opcao == "D":
         deposito = int(input("Digite o valor que deseja depositar: "))
         while deposito <= 0:
